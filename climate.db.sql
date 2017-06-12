71365cd85c9c01287dd1581eff1092f1fd1ef60f
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-06-12 14:34:48 -0500

    Commands to create database

---
+++ climate.db.sql
@@ -0,0 +1,12 @@
+su - postgres
+psql
+CREATE USER "sbcuser" WITH PASSWORD 's8cUr';
+CREATE DATABASE "climatedb" WITH OWNER "sbcuser"; 
+\q
+psql -U postgres "climatedb"
+CREATE SCHEMA "climateapp" authorization "sbcuser";
+ALTER USER "sbcuser" SET search_path TO "climateapp";
+\q
+psql -U "sbcuser" "climatedb"
+
+select current_schema();
