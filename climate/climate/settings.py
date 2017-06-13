a69905558cf3282bdfe0bb891adffd6a440cd3f4
madba <xxmadbaxx@gmail.com>
2017-06-13 15:17:14 -0500

    inicio pagina django

--- climate/climate/settings.py
+++ climate/climate/settings.py
@@ -105,10 +105,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
+LANGUAGE_CODE = 'es-ec'
 
-LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'America/Guayaquil'
 
-TIME_ZONE = 'UTC'
+#LANGUAGE_CODE = 'en-us'
+
+#TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
